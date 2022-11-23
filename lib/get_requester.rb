e1847b6931093d0541726f7429e991ef57e18d88
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-23 23:07:03 +1100

    Build a reusable class for sending requests and handling responses

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    #convert JSON returned from 'get_response_body' into Ruby data structures
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+    end
+
+end
